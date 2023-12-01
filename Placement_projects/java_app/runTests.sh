75c6ee79e017a83bb59fdcba055f2bace3442acd
Shane Ginty <shane.ginty@siemens.com>
2023-12-01 16:05:35 +0000

    Added shell script for compiling and running tests

---
+++ Placement_projects/java_app/runTests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Remove any old logfile
+path_to_log_file="TestOutputFile.log"
+
+# Check file exists before deleting (good practise)
+
+if [ -e "$path_to_log_file" ]; then
+    rm "$path_to_log_file"
+    echo "Deleted old log file"
+fi
+
+javac test_folder/testing.java  # Compile
+java test_folder.testing    # Run
