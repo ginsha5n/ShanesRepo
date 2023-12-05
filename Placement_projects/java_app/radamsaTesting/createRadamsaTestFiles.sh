7b479a4eedb417e733c6c8d28ecc628024455f16
Shane Ginty <shane.ginty@siemens.com>
2023-12-05 16:14:52 +0000

    cleaning up class files

--- Placement_projects/java_app/radamsaTesting/createRadamsaTestFiles.sh
+++ Placement_projects/java_app/radamsaTesting/createRadamsaTestFiles.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+default_files_created=10
+
+# This takes in the first argument in th cli as the amount of files to create, 
+# if none is provided it defaults to the default_files_created value
+files_created=${1:-$default_files_created}
+
 # Delete old test files
 rm radamsaOutputFiles/file*
 
 # Create new files using radamsa
-radamsa -n 10 -o radamsaOutputFiles/file%n.txt ValidInput.txt
+radamsa -n "$files_created" -o radamsaOutputFiles/file%n.txt ValidInput.txt
