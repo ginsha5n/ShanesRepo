37364b95dbbe89fb523cc6f971982b9ef3b53a0d
Shane Ginty <shane.ginty@siemens.com>
2023-12-06 10:33:34 +0000

    Added comments to runRadamsaFiles.sh

--- Placement_projects/java_app/scripts/runRadamsaFiles.sh
+++ Placement_projects/java_app/scripts/runRadamsaFiles.sh
@@ -4,6 +4,7 @@ cd ..
 
 directory="radamsaTesting/radamsaOutputFiles/"
 
+# Counter for tests that passed/failed
 tests_passed=0
 tests_failed=0
 
@@ -13,6 +14,7 @@ for file in "$directory"*; do
     java src.app -f $file
     exit_code=$?
 
+    # if process exit code equals zero increase tests passed value else increse tests failed value
     if [ "$exit_code" -eq 0 ]; then
         echo "$file passed"
         ((tests_passed++))
@@ -23,5 +25,6 @@ for file in "$directory"*; do
     fi
 done
 
+# Print amount of tests passed and failed.
 echo "Passed: $tests_passed"
 echo "Failed: $tests_failed"
